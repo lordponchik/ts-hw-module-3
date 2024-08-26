0d709ba09275bdb697e0e1eab2bfad3833acabfb
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-26 13:32:32 +0200

    task-4

--- src/main.ts
+++ src/main.ts
@@ -71,4 +71,61 @@ wizard.introduce("I am the mighty wizard, Merlin");
 wizard.castSpell();
 wizard.levelUp(); // Level up! New level is 16
 
+// task-04
+
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
+
+console.log(key.getSignature());
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+console.log(person.getKey());
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
 export {};
