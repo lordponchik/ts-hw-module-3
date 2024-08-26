537986f7d0d7a1e09fbebd1fe64b80a35c0b7cc4
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-26 12:54:11 +0200

    add task-3

--- src/main.ts
+++ src/main.ts
@@ -30,4 +30,45 @@ class Manager extends Employee {
   }
 }
 
+// task-03
+
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+
+class Wizard implements ICharacter, ISpellCaster {
+  constructor(public name: string, public level: number) {
+    if (this.level < 1) {
+      throw new Error("Level too low");
+    }
+  }
+
+  introduce(phrase: string): void {
+    console.log(phrase + ", " + this.name);
+  }
+
+  castSpell(): void {
+    console.log("Casting a spell, behold my power!");
+  }
+
+  levelUp(): void {
+    this.level++;
+    console.log(`Level up! New level is ${this.level}`);
+  }
+}
+
+// тестування класу
+const wizard = new Wizard("Merlin", 15);
+
+wizard.introduce("I am the mighty wizard, Merlin");
+wizard.castSpell();
+wizard.levelUp(); // Level up! New level is 16
+
 export {};
