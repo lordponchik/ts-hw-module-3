fb0b6cf626286355c5f8c80dc3b48100dd0aca53
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-26 12:48:42 +0200

    add task-2

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,30 @@ class Student {
   constructor(public name: string, public age: number, public grade: string) {}
 }
 
+// task-02
+
+class Employee {
+  // Заповніть модифікатори доступу
+  public name: string;
+  private department: string;
+  protected salary: number;
+
+  constructor(name: string, department: string, salary: number) {
+    this.name = name;
+    this.department = department;
+    this.salary = salary;
+  }
+
+  getEmployeeDetails() {
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
+}
+
+class Manager extends Employee {
+  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
+}
+
 export {};
